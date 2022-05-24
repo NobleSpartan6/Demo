46641ab379af6c23813a8ab9144c8b98653157f5
Ebenezer <ese49@cornell.edu>
2022-05-24 04:41:29 -0400

    Experimented with Views and Layouts. Seekbar changes text to the max progess number of the bar.

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var seekbar: SeekBar
+    private lateinit var textview: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekbar = findViewById(R.id.seekbar)
+        textview = findViewById(R.id.textview)
+
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textview.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
     }
 }
